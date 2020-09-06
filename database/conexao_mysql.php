b3d9ae6a0e5bfe121331a2573e2598867de9824e
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-09-06 15:42:33 -0300

    Alteração no Banco de Dados
    Atualização do Banco de Dados e no nome de alguns campos

--- database/conexao_mysql.php
+++ database/conexao_mysql.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conexao = mysqli_connect("localhost", "root", "", "salao");
+	$conexao = mysqli_connect("localhost", "root", "", "tcc");
 	
 	if (!mysqli_set_charset($conexao, "utf8")) {
         printf("Error loading character set utf8: %s\n", mysqli_error($link));
--- php/galeria_imagens.php
+++ php/galeria_imagens.php
@@ -5,7 +5,7 @@
     $resultado = mysqli_query($conexao, $sql);
 
     while($result=mysqli_fetch_array($resultado)){
-        echo '<div style="background-image: url(data:image/jpeg;base64,'.base64_encode( $result['imagem'] ).');" class="banner-single" ></div>';
+        echo '<div style="background-image: url(data:image/jpeg;base64,'.base64_encode( $result['tamanho'] ).');" class="banner-single" ></div>';
     }
 
 
