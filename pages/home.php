ab86beba775cb56694bb774d6163e96582d6056c
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-10-08 18:20:15 -0300

    Enviar Email
    Funcionalidade para o cliente enviar um email com sugestões ou críticas

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,63 @@
+<?php
+
+    // Import PHPMailer classes into the global namespace
+    // These must be at the top of your script, not inside a function
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    // Load Composer's autoloader
+    require 'vendor/autoload.php';
+
+    if(isset($_POST["action"])){
+
+		$nome = $_POST["nome"];
+		$email = $_POST["email"];
+		$tel = $_POST["tel"];
+        $mens = str_replace("\n", "<br>",$_POST["mens"]);
+
+        // Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+            //Server settings
+            //$mail->SMTPDebug = 3;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'paulovictorsantos0@gmail.com';                     // SMTP username
+            $mail->Password   = 'zcnnckhjposdwziy';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom("$email", "$nome");
+            $mail->addAddress('paulovictorsantos0@gmail.com');
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Sugestão ou dúvida de '.$nome;
+            $mail->Body    = "<br>Nome: $nome<br>Email: $email<br>Tel: $tel<hr>$mens";
+            $mail->AltBody = "$mens";
+
+            $mail->send();
+            echo "<div class='mensagem green'>E-mail enviado com sucesso</div>";
+        } catch (Exception $e) {
+            echo "<div class='mensagem red'>Erro ao enviar o e-mail, por favor tente mais tarde</div>";
+        }
+    }
+
+?>
+
 <section class="home">
 		<div class="banner">
 			<div class="text">
@@ -42,15 +102,15 @@
 				<p>Tel: (19) 3838-4424</p><br>
 				<p>HORÁRIO DE FUNCIONAMENTO</p>
 				<p>Ter - Sex: 8:00 - 20:00<br>Sábado: 8:00 - 22:00</p><br>
-				<form action="" method="GET">
+				<form action="<?php echo INCLUDE_PATH; ?>" method="POST">
 					<div id="divInput" class="w50 left">
-						<input type="text" name="nomeForm" placeholder="Nome" class="left">
-						<input type="text" name="emailForm" placeholder="Email" class="left">
-						<input type="text" name="telForm" placeholder="Telefone" class="left">
+						<input type="text" name="nome" placeholder="Nome" class="left">
+						<input type="text" name="email" placeholder="Email" class="left">
+						<input type="text" name="tel" placeholder="Telefone" class="left">
 					</div>
-					<textarea id="mensagem" class="w50 right" name="mensagem" placeholder="Digite sua mensagem aqui..."></textarea>
+					<textarea id="mensagem" class="w50 right" name="mens" placeholder="Digite sua mensagem aqui..."></textarea>
 					<div class="clear"></div>
-					<input type="submit" value='Enviar' class="right">
+					<input type="submit" name="action" value='Enviar' class="right">
 				</form>
 				<div class="clear"></div>
 				<a href="<?php echo INCLUDE_PATH; ?>mapa">Como chegar?</a>
