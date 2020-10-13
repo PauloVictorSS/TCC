142073956331bde0bb6c31f590b6a3906535d63b
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-10-13 18:48:55 -0300

    CSS - Gerenciamento das Páginas
    CSS da página que possibilita o funcionário mudar os conteúdos das páginas do site

--- painel/pages/gerenciar_paginas.php
+++ painel/pages/gerenciar_paginas.php
@@ -1,31 +1,45 @@
 Gerenciar Paginas
 <hr>
 <br>
+<div class="paginas">
+    <table>
+        <tr>
+            <th>Nome</th>
+            <th>Conteúdo</th>
+            <th>Alteração</th>
+        </tr>
+        <?php
+            if(isset($_POST["action"])){
 
-    <?php
-        if(isset($_POST["action"])){
+                $select = "SELECT * FROM textos WHERE id = ".$_POST['action']." and texto = '".$_POST["newText"]."'";
 
-            $alterar = "UPDATE textos SET texto ='".$_POST['newText']."' WHERE id =".$_POST['action'];
-    
-            $resultado = mysqli_query($conexao, $alterar);
+                $resultado1 = mysqli_query($conexao, $select);
 
-            if($resultado != 0){
-                echo "<div class='mensagem green'>Texto alterado com sucesso</div>";
-            }
-    
-        }
+                if(mysqli_num_rows($resultado1) != 1){
 
-        $consulta = "SELECT * FROM textos ORDER BY titulo";
-        $result = mysqli_query($conexao, $consulta);
-
-        while($lista = mysqli_fetch_array($result)){
+                    $alterar = "UPDATE textos SET texto ='".$_POST["newText"]."' WHERE id =".$_POST['action'];
             
-            echo "<div>";
-                echo "<p>".$lista['titulo']."</p>";
-                echo "<form action='Gerenciar-Paginas' method='POST'>";
-                    echo "<input type='text' name='newText' value='".$lista['texto']."'></input><br>";
-                    echo "<button name='action' value='".$lista['id']."' class='right'>Salvar</button>";
-                echo "</form>";
-            echo "</div>";
-        }
-    ?>
+                    $resultado2 = mysqli_query($conexao, $alterar);
+
+                    if(mysqli_affected_rows($conexao) != 0)
+                        echo "<div class='mensagem green'>Texto alterado com sucesso</div>";
+                    else
+                        echo "<div class='mensagem red'>Houve algum erro</div>";
+                }
+            }
+
+            $consulta = "SELECT * FROM textos ORDER BY titulo";
+            $result = mysqli_query($conexao, $consulta);
+
+            while($lista = mysqli_fetch_array($result)){
+
+                echo "<tr><form action='Gerenciar-Paginas' method='POST' class='form_infs'>";
+                echo "<td>".$lista["titulo"]."</td>";
+                echo "<td><textarea name='newText'>".$lista['texto']."</textarea></td>";
+                echo "<td><button name='action' value='".$lista['id']."' class='green'>Salvar</button></td></form></tr>";
+                
+            }
+        ?>
+
+    </table>
+</div>
