69984aa3b1e6c859f32747d8fa708d59ef606304
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-10-21 20:02:42 -0300

    Funcionalidade de Alteração de Horários
    Adição da funcionalidade para alterar os horários disponíveis

--- painel/pages/gerenciar_paginas.php
+++ painel/pages/gerenciar_paginas.php
@@ -42,4 +42,47 @@ Gerenciar Paginas
         ?>
 
     </table>
+    <br><br>
+    Horários de entrada e saída
+    <br><br>
+    <table class="infs_horarios">
+        <tr>
+            <th>Dia da Semana</th>
+            <th>Entrada e saída</th>
+            <th>Alteração</th>
+        </tr>
+        <?php
+            if(isset($_POST["action2"])){
+
+                $select = "SELECT * FROM horarios WHERE id = ".$_POST['action2']." and hora_inicio = '".$_POST["newInicio"]."' and hora_termino = '".$_POST["newTermino"]."'";
+
+                $resultado1 = mysqli_query($conexao, $select);
+
+                if(mysqli_num_rows($resultado1) != 1){
+
+                    $alterar = "UPDATE horarios SET hora_inicio ='".$_POST["newInicio"]."', hora_termino ='".$_POST["newTermino"]."' WHERE id =".$_POST['action2'];
+            
+                    $resultado2 = mysqli_query($conexao, $alterar);
+
+                    if(mysqli_affected_rows($conexao) != 0)
+                        echo "<div class='mensagem green'>Texto alterado com sucesso</div>";
+                    else
+                        echo "<div class='mensagem red'>Houve algum erro</div>";
+                }
+            }
+
+            $consulta = "SELECT * FROM horarios";
+            $result = mysqli_query($conexao, $consulta);
+
+            while($lista = mysqli_fetch_array($result)){
+
+                echo "<tr><form action='Gerenciar-Paginas' method='POST' class='form_infs'>";
+                echo "<td>".$lista["dia"]."</td>";
+                echo "<td><div class='input'>Início:<br><input name='newInicio' value='".$lista['hora_inicio']."'><br></div></div><div class='input'>Saída:<br><input name='newTermino' value='".$lista['hora_termino']."'></div></td>";
+                echo "<td><button name='action2' value='".$lista['id']."' class='green'>Salvar</button></td></form></tr>";
+                
+            }
+        ?>
+    </table>
+    <aside>* Caso não tenha trabalho no dia, coloque o valor 0 *</aside>
 </div>
