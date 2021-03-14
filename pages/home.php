2fb1a118026eae91b476004289ce091b892ab899
Paulo Victor de Souza Santos <50722782+PauloVictorSS@users.noreply.github.com>
2021-03-14 15:21:08 -0300

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -38,8 +38,8 @@
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'paulovictorsantos0@gmail.com';                     // SMTP username
-            $mail->Password   = 'zcnnckhjposdwziy';                               // SMTP password
+            $mail->Username   = 'email@gmail.com';                     // SMTP username
+            $mail->Password   = 'senha123';                               // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
